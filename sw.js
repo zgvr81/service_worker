b477c8d2bf9fd3476d282a6141ed59918ca2cf63
zgvr81 <hddr09@gmail.com>
2022-08-26 13:53:48 +0200

    init

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/service_worker/',
+       '/service_worker/index.html',
+       '/service_worker/main.js',
+       '/service_worker/style.css'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
